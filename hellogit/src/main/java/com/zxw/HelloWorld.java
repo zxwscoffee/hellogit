0abcef2fda65a9e8f6852e9101b25188a2c4cb4b
zxw <2840088759@qq.com>
2021-02-04 18:46:52 +0800

    master修改

--- hellogit/src/main/java/com/zxw/HelloWorld.java
+++ hellogit/src/main/java/com/zxw/HelloWorld.java
@@ -6,5 +6,6 @@ package com.zxw;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
+        System.out.println("master change");
     }
 }
