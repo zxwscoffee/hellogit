7a2036e9474fb1427b00e9d0c58be2a6d900129b
zxw <2840088759@qq.com>
2021-02-04 18:32:17 +0800

    将World修改为Git

--- hellogit/src/main/java/com/zxw/HelloWorld.java
+++ hellogit/src/main/java/com/zxw/HelloWorld.java
@@ -5,6 +5,6 @@ package com.zxw;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, Git");
     }
 }
