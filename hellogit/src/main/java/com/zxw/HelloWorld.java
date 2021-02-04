379c70ad291c86c9ee4b4af154d01acf3d151518
zxw <2840088759@qq.com>
2021-02-04 18:47:16 +0800

    hot_fix修改

--- hellogit/src/main/java/com/zxw/HelloWorld.java
+++ hellogit/src/main/java/com/zxw/HelloWorld.java
@@ -6,5 +6,6 @@ package com.zxw;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Git");
+        System.out.println("hot_fix修改");
     }
 }
